dbbef0b855af33dab4e7f36a8bad02c515f503d6
Dominic Black <me@jdm.black>
2017-12-11 12:50:09 +0000

    Day 11 - Part 2 Completed

--- day-11/src/main.rs
+++ day-11/src/main.rs
@@ -2,7 +2,9 @@
 fn main() {
     static INPUT: &str = include_str!("input.txt");
 
-    println!("Part 1: {}", distance(INPUT));
+    let (distance_now, max_distance) = distance(INPUT);
+    println!("Part 1: {}", distance_now);
+    println!("Part 2: {}", max_distance);
 }
 
 /*
@@ -28,11 +30,11 @@ Grid Coordinates (which are flat and have constant movement)
 */
 
 /// Calculates the shortest number of steps to get back
-fn distance(input: &str) -> i32 {
-    let pos: (i32, i32) = input.trim().split(',').fold(
-        (0, 0),
-        | current, mov | {
-            match mov {
+fn distance(input: &str) -> (i32, i32) {
+    let (max_distance, pos) = input.trim().split(',').fold(
+        (0, (0, 0)),
+        | (max_distance, current), mov | {
+            let new_position: (i32, i32) = match mov {
                 "n"  => (current.0,     current.1 + 1),
                 "s"  => (current.0,     current.1 - 1),
                 "ne" => (current.0 + 1, current.1 + 1),
@@ -40,12 +42,17 @@ fn distance(input: &str) -> i32 {
                 "nw" => (current.0 - 1, current.1),
                 "se" => (current.0 + 1, current.1),
                 _    => panic!("Unknown movement {}", mov),
-            }
+            };
+
+            let new_position_distance = new_position.0.abs().max(new_position.1.abs());
+
+            (max_distance.max(new_position_distance), new_position)
         }
     );
 
     // Because our grid is flat, it's simply the largest of these two
-    pos.0.abs().max(pos.1.abs())
+    let distance_now = pos.0.abs().max(pos.1.abs());
+    (distance_now, max_distance)
 }
 
 #[cfg(test)]
@@ -54,9 +61,9 @@ mod test {
 
     #[test]
     fn test_distance() {
-        assert_eq!(distance("ne,ne,ne"), 3, "ne,ne,ne");
-        assert_eq!(distance("ne,ne,sw,sw"), 0, "ne,ne,sw,sw");
-        assert_eq!(distance("ne,ne,s,s"), 2, "ne,ne,s,s");
-        assert_eq!(distance("se,sw,se,sw,sw"), 3, "se,sw,se,sw,sw");
+        assert_eq!(distance("ne,ne,ne").0, 3, "ne,ne,ne");
+        assert_eq!(distance("ne,ne,sw,sw").0, 0, "ne,ne,sw,sw");
+        assert_eq!(distance("ne,ne,s,s").0, 2, "ne,ne,s,s");
+        assert_eq!(distance("se,sw,se,sw,sw").0, 3, "se,sw,se,sw,sw");
     }
 }
